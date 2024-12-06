781d4ba6014fa184953b8b87699b4a22293b234f
THSayeb <desktop499@gmail.com>
2024-12-06 23:36:47 +0600

    Square_root

---
+++ Square_root.py
@@ -0,0 +1,6 @@
+print("welcome to square root calculator \n enter numbers to find their square root")
+
+num = float(input("Enter a number "))
+ans = num**0.5
+
+print("\n Square root of the number is ", ans, "\n")
